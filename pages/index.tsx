1b251af8509c026a11eca16d2fc1b401fb2920f8
vipzero <hiroflighter@gmail.com>
2019-10-19 00:14:47 +0900

    Initialize commit

---
+++ pages/index.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { Container, CssBaseline, Typography, Button } from '@material-ui/core'
+import firebase from '../utils/firebase'
+
+const db = firebase.firestore()
+
+type Props = {}
+
+const WithInitialProps: NextPage<Props> = () => {
+  const [storyId, setStoryId] = useState<number | null>(null)
+
+  useEffect(() => {
+    db.collection('all')
+      .doc('info')
+      .get()
+      .then(snap => {
+        if (!snap.exists) {
+          return
+        }
+        const info = snap.data()
+        const count = (info && info.count) || 0
+        const storyId = Math.floor(Math.random() * count)
+
+        setStoryId(storyId)
+      })
+  }, [])
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div>
+        <Typography style={{ padding: '16px 0' }} component="h1" variant="h5">
+          億万長者話題ボックス
+        </Typography>
+        <Typography style={{ padding: '16px 0' }} component="h2" variant="h5">
+          話題にどうぞ。5ch VIP からどちらを選ぶかネタを収集。
+        </Typography>
+        {storyId === null ? (
+          <Typography>ロード中</Typography>
+        ) : (
+          <Link href={`/story/${storyId}`}>
+            <Button fullWidth color="primary" size="large" variant="outlined">
+              話題を引く
+            </Button>
+          </Link>
+        )}
+      </div>
+    </Container>
+  )
+}
+
+export default WithInitialProps
---
+++ pages/story/Story.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import { Container, Typography, Button } from '@material-ui/core'
+import { useRouter } from 'next/router'
+
+import Link from 'next/link'
+import firebase from '../../utils/firebase'
+
+const db = firebase.firestore()
+
+type Props = {}
+
+function Story() {
+  const { id } = useRouter().query
+  const storyId = typeof id === 'string' ? id : '---'
+
+  const [title, setTitle] = React.useState<string | null>(null)
+
+  React.useEffect(() => {
+    db.collection('stories')
+      .where('id', '==', Number(storyId))
+      .get()
+      .then(snap => {
+        snap.forEach(doc => {
+          if (!doc.exists) {
+            return
+          }
+          const data = doc.data()
+
+          console.log({ data })
+          const title = (data && data.title) || 'none'
+
+          setTitle(title)
+        })
+      })
+  }, [storyId])
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Typography style={{ padding: '16px 0' }} variant="h5" component="h1">
+        {title}
+      </Typography>
+      <Link href={`/`}>
+        <Button fullWidth size="large" variant="outlined">
+          戻る
+        </Button>
+      </Link>
+    </Container>
+  )
+}
+
+export default Story
